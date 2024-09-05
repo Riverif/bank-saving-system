94e6d06a9a2931858f5a5f73a14beb9ce7b7cb32
Riverif <rifkialfiann@gmail.com>
2024-09-05 16:50:33 +0700

    2.4 debug

--- app/(root)/_components/customers.tsx
+++ app/(root)/_components/customers.tsx
@@ -2,7 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import { Customer } from "@prisma/client";
+import axios from "axios";
+import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface CustomersProps {
   customers: Customer[];
@@ -10,14 +13,31 @@ interface CustomersProps {
 export const Customers = ({ customers }: CustomersProps) => {
   const router = useRouter();
 
-  router.refresh();
+  const [data, setData] = useState<Customer[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/customers");
+        setData(response.data);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="space-y-2">
       <p className="text-center">choose customer</p>
-      <div className="flex flex-wrap gap-4  max-w-[800px] ">
-        {customers ? (
-          customers.map((customer) => (
+      <div className="flex flex-wrap gap-4  max-w-[800px] justify-center">
+        {data ? (
+          data.map((customer) => (
             <Button
               key={customer.id}
               variant="secondary"
@@ -30,7 +50,9 @@ export const Customers = ({ customers }: CustomersProps) => {
             </Button>
           ))
         ) : (
-          <div className="animate-spin">test</div>
+          <div className="animate-spin">
+            <Loader />
+          </div>
         )}
       </div>
     </div>
